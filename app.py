04102edff0c4e7c614cc084abc4de4ba6346184d
TTDUNG-3864 <konhotk206@gmail.com>
2026-03-06 17:16:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -240,6 +240,8 @@ def save_result():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # Mở cổng (Port) chuẩn cho Render
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
